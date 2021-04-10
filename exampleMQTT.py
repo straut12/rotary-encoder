593880099b440d41ecaff80ab4cc073b0ddc22f5
stemjust4u <stemjust4u@gmail.com>
2021-04-10 17:12:45 -0600

    updated to main layout

--- exampleMQTT.py
+++ exampleMQTT.py
@@ -9,27 +9,65 @@ import paho.mqtt.client as mqtt
 from os import path
 from pathlib import Path
 import rotaryencoder
-
-if __name__ == "__main__":
     
-    def is_integer(n):
-        if n == None:
-            return False
-        try:
-            float(n)
-        except ValueError:
-            return False
-        else:
-            return float(n).is_integer()
-
-    ''' alternative integer check
-    def is_integer_num(n):
-        if isinstance(n, int):
-            return True
-        if isinstance(n, float):
-            return n.is_integer()
+def is_integer(n):
+    if n == None:
         return False
-    '''
+    try:
+        float(n)
+    except ValueError:
+        return False
+    else:
+        return float(n).is_integer()
+
+''' alternative integer check
+def is_integer_num(n):
+    if isinstance(n, int):
+        return True
+    if isinstance(n, float):
+        return n.is_integer()
+    return False
+'''
+
+def on_connect(client, userdata, flags, rc):
+    """ on connect callback verifies a connection established and subscribe to TOPICs"""
+    logging.info("attempting on_connect")
+    if rc==0:
+        mqtt_client.connected = True          # If rc = 0 then successful connection
+        client.subscribe(MQTT_SUB_TOPIC1)      # Subscribe to topic
+        logging.info("Successful Connection: {0}".format(str(rc)))
+        logging.info("Subscribed to: {0}\n".format(MQTT_SUB_TOPIC1))
+    else:
+        mqtt_client.failed_connection = True  # If rc != 0 then failed to connect. Set flag to stop mqtt loop
+        logging.info("Unsuccessful Connection - Code {0}".format(str(rc)))
+
+def on_message(client, userdata, msg):
+    """on message callback will receive messages from the server/broker. Must be subscribed to the topic in on_connect"""
+    if msg.topic == MQTT_SUB_TOPIC1:
+        incomingD = json.loads(str(msg.payload.decode("utf-8", "ignore")))  # decode the json msg and convert to python dictionary
+        newmsg = True
+        # Debugging. Will print the JSON incoming payload and unpack the converted dictionary
+        logging.debug("Receive: msg on subscribed topic: {0} with payload: {1}".format(msg.topic, str(msg.payload))) 
+        logging.debug("Incoming msg converted (JSON->Dictionary) and unpacking")
+        for key, value in incomingD.items():
+            logging.debug("{0}:{1}".format(key, value))
+
+def on_publish(client, userdata, mid):
+    """on publish will send data to client"""
+    #Debugging. Will unpack the dictionary and then the converted JSON payload
+    logging.debug("msg ID: " + str(mid)) 
+    logging.debug("Publish: Unpack outgoing dictionary (Will convert dictionary->JSON)")
+    for key, value in outgoingD.items():
+        logging.debug("{0}:{1}".format(key, value))
+    logging.debug("Converted msg published on topic: {0} with JSON payload: {1}\n".format(MQTT_PUB_TOPIC1, json.dumps(outgoingD))) # Uncomment for debugging. Will print the JSON incoming msg
+    pass 
+
+def on_disconnect(client, userdata,rc=0):
+    logging.debug("DisConnected result code "+str(rc))
+    mqtt_client.loop_stop()
+
+def main():
+    global mqtt_client, MQTT_SUB_TOPIC1, MQTT_PUB_TOPIC1, outgoingD, incomingD, newmsg
 
     logging.basicConfig(level=logging.DEBUG) # Set to CRITICAL to turn logging off. Set to DEBUG to get variables. Set to INFO for status messages.
 
@@ -39,51 +77,12 @@ if __name__ == "__main__":
     with open(path.join(home, "stem"),"r") as f:
         stem = f.read().splitlines()
 
-    #=======   SETUP MQTT =================#
     MQTT_SERVER = '10.0.0.115'                    # Replace with IP address of device running mqtt server/broker
     MQTT_USER = stem[0]                           # Replace with your mqtt user ID
     MQTT_PASSWORD = stem[1]                       # Replace with your mqtt password
     MQTT_CLIENT_ID = 'RPi4'
     MQTT_SUB_TOPIC1 = 'RPi/rotenc/all'
     MQTT_PUB_TOPIC1 = 'RPi/rotenc'
-
-    def on_connect(client, userdata, flags, rc):
-        """ on connect callback verifies a connection established and subscribe to TOPICs"""
-        logging.info("attempting on_connect")
-        if rc==0:
-            mqtt_client.connected = True          # If rc = 0 then successful connection
-            client.subscribe(MQTT_SUB_TOPIC1)      # Subscribe to topic
-            logging.info("Successful Connection: {0}".format(str(rc)))
-            logging.info("Subscribed to: {0}\n".format(MQTT_SUB_TOPIC1))
-        else:
-            mqtt_client.failed_connection = True  # If rc != 0 then failed to connect. Set flag to stop mqtt loop
-            logging.info("Unsuccessful Connection - Code {0}".format(str(rc)))
-
-    def on_message(client, userdata, msg):
-        """on message callback will receive messages from the server/broker. Must be subscribed to the topic in on_connect"""
-        global newmsg, incomingD
-        if msg.topic == MQTT_SUB_TOPIC1:
-            incomingD = json.loads(str(msg.payload.decode("utf-8", "ignore")))  # decode the json msg and convert to python dictionary
-            newmsg = True
-            # Debugging. Will print the JSON incoming payload and unpack the converted dictionary
-            logging.debug("Receive: msg on subscribed topic: {0} with payload: {1}".format(msg.topic, str(msg.payload))) 
-            logging.debug("Incoming msg converted (JSON->Dictionary) and unpacking")
-            for key, value in incomingD.items():
-                logging.debug("{0}:{1}".format(key, value))
-
-    def on_publish(client, userdata, mid):
-        """on publish will send data to client"""
-        #Debugging. Will unpack the dictionary and then the converted JSON payload
-        logging.debug("msg ID: " + str(mid)) 
-        logging.debug("Publish: Unpack outgoing dictionary (Will convert dictionary->JSON)")
-        for key, value in outgoingD.items():
-            logging.debug("{0}:{1}".format(key, value))
-        logging.debug("Converted msg published on topic: {0} with JSON payload: {1}\n".format(MQTT_PUB_TOPIC1, json.dumps(outgoingD))) # Uncomment for debugging. Will print the JSON incoming msg
-        pass 
-
-    def on_disconnect(client, userdata,rc=0):
-        logging.debug("DisConnected result code "+str(rc))
-        mqtt_client.loop_stop()
 
     #==== start/bind mqtt functions ===========#
     # Create a couple flags to handle a failed attempt at connecting. If user/password is wrong we want to stop the loop.
@@ -126,3 +125,6 @@ if __name__ == "__main__":
     finally:
         GPIO.cleanup()
         logging.info("GPIO cleaned up")
+
+if __name__ == "__main__":
+    main()
